cd861dd427de1ef0b3da55689ee71ceece32235e
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-04-27 02:54:49 +0530

    ingre

--- main.tf
+++ main.tf
@@ -68,6 +68,8 @@ module "security_group" {
   vpc_id      = module.vpc.vpc_id
 
   ingress_rules            = ["all-tcp"]
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
   egress_rules             = ["all-tcp"]
-  cidr_blocks = "0.0.0.0/0"
+  egress_cidr_blocks      = ["0.0.0.0/0"]
+
 }
