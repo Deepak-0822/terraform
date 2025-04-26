4db5e10668cecb12d4e4492e654e7f6d63a5c936
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-04-27 02:38:59 +0530

    out

--- main.tf
+++ main.tf
@@ -65,7 +65,7 @@ module "security-group" {
   source = "./modules/security-group"
 
   name        = "user-service"
-  vpc_id      = module.vpc.aws_vpc.this[0].id
+  vpc_id      = module.vpc.vpc_id
 
   ingress_rules            = ["all-all"]
   egress_rules             = ["all-all"]
