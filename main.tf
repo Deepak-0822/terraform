5fd52f7617a624fc47d1ea06d5e7cbe59e069f66
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-04-27 16:18:06 +0530

    import

--- main.tf
+++ main.tf
@@ -73,3 +73,35 @@ module "security_group" {
   egress_cidr_blocks      = ["0.0.0.0/0"]
 
 }
+
+resource "aws_iam_role" "test_role" {
+  name = "ec2-role-import" # It's good practice to have descriptive names
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  # Attach the AdministratorAccess policy to give full permissions
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AdministratorAccess",
+  ]
+
+  tags = {
+    tag-key = "tag-value"
+    Purpose = "Administrator Role for EC2" # Add a descriptive tag
+  }
+}
+
+## aws_iam_role.test_role ec2-role-import
