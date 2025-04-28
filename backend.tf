6d071ff45f3dec1f79e02aec86aa811f7874c12c
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-04-28 23:20:30 +0530

    hcp

--- backend.tf
+++ backend.tf
@@ -1,9 +1,19 @@
+#terraform {
+ # backend "s3" {
+  #  bucket       = "demo-tfstate-testing"
+   # key          = "path/to/state"
+    # region       = "ap-south-1"
+   # use_lockfile = true
+   # encrypt = true 
+ # }
+# }
+
 terraform {
-  backend "s3" {
-    bucket       = "demo-tfstate-testing"
-    key          = "path/to/state"
-    region       = "ap-south-1"
-    use_lockfile = true
-    encrypt = true 
+  backend "remote" {
+    organization = Org-new
+    
+    workspaces {
+      name = "terraform"
+    }
   }
 }
