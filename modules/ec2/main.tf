42bc9d268d9bc0ddc4624c3ee4df1baad68807f6
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-03 00:43:13 +0530

    cr

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -5,13 +5,6 @@ locals {
   is_t_instance_type = replace(var.instance_type, "/^t(2|3|3a|4g){1}\\..*$/", "1") == "1" ? true : false
 }
 
-data "aws_ssm_parameter" "this" {
-  count = local.create && var.ami == null ? 1 : 0
-
-  name = var.ami_ssm_parameter
-}
-
-
 ################################################################################
 # Instance
 ################################################################################
