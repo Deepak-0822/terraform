e16d75e64e391d1e5fc0dcc5ecca5f339960e046
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-02 15:28:00 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,54 +1,42 @@
+########################################
+# Locals
+########################################
+
 locals {
-
-  len_public_subnets      = max(length(var.public_subnets))
-
-  max_subnet_length = max(
-    local.len_public_subnets
-  )
-    # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
-  vpc_id = try( aws_vpc.this[0].id, "")
-
-  create_vpc = var.create_vpc
+  len_public_subnets      = length(var.public_subnets)
+  vpc_id                  = try(aws_vpc.this[0].id, "")
+  create_public_subnets   = var.create_vpc && local.len_public_subnets > 0
+  num_public_route_tables = var.create_multiple_public_route_tables ? local.len_public_subnets : 1
 }
 
-
-
-
-################################################################################
+########################################
 # VPC
-################################################################################
+########################################
 
 resource "aws_vpc" "this" {
-  count = local.create_vpc ? 1 : 0
-
-  cidr_block          = var.cidr
-
-  instance_tenancy                     = var.instance_tenancy
-  enable_dns_hostnames                 = var.enable_dns_hostnames
-  enable_dns_support                   = var.enable_dns_support
+  count                = var.create_vpc ? 1 : 0
+  cidr_block           = var.cidr
+  instance_tenancy     = var.instance_tenancy
+  enable_dns_hostnames = var.enable_dns_hostnames
+  enable_dns_support   = var.enable_dns_support
 
   tags = merge(
     { "Name" = var.name },
     var.tags,
-    var.vpc_tags,
+    var.vpc_tags
   )
 }
 
-################################################################################
-# Publiс Subnets
-################################################################################
-
-locals {
-  create_public_subnets = local.create_vpc && local.len_public_subnets > 0
-}
+########################################
+# Public Subnets
+########################################
 
 resource "aws_subnet" "public" {
-  count = local.create_public_subnets
+  count = local.create_public_subnets ? local.len_public_subnets : 0
 
-  availability_zone                              = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
-  availability_zone_id                           = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
-  cidr_block                                     = element(concat(var.public_subnets, [""]), count.index)
-  vpc_id                                         = local.vpc_id
+  availability_zone    = element(var.azs, count.index)
+  cidr_block           = element(var.public_subnets, count.index)
+  vpc_id               = local.vpc_id
 
   tags = merge(
     {
@@ -63,33 +51,37 @@ resource "aws_subnet" "public" {
   )
 }
 
-locals {
-  num_public_route_tables = var.create_multiple_public_route_tables ? local.len_public_subnets : 1
-}
+########################################
+# Public Route Tables
+########################################
 
 resource "aws_route_table" "public" {
-  count = local.create_public_subnets ? local.num_public_route_tables : 0
-
+  count  = local.create_public_subnets ? local.num_public_route_tables : 0
   vpc_id = local.vpc_id
 
   tags = merge(
     {
-      "Name" = var.create_multiple_public_route_tables ? format(
-        "${var.name}-${var.public_subnet_suffix}-%s",
-        element(var.azs, count.index),
-      ) : "${var.name}-${var.public_subnet_suffix}"
+      Name = var.create_multiple_public_route_tables ?
+        format("${var.name}-${var.public_subnet_suffix}-%s", element(var.azs, count.index)) :
+        "${var.name}-${var.public_subnet_suffix}"
     },
     var.tags,
-    var.public_route_table_tags,
+    var.public_route_table_tags
   )
 }
 
 resource "aws_route_table_association" "public" {
   count = local.create_public_subnets ? local.len_public_subnets : 0
 
-  subnet_id      = element(aws_subnet.public[*].id, count.index)
-  route_table_id = element(aws_route_table.public[*].id, var.create_multiple_public_route_tables ? count.index : 0)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = var.create_multiple_public_route_tables ?
+    aws_route_table.public[count.index].id :
+    aws_route_table.public[0].id
 }
+
+########################################
+# Public Route to Internet Gateway
+########################################
 
 resource "aws_route" "public_internet_gateway" {
   count = local.create_public_subnets && var.create_igw ? local.num_public_route_tables : 0
@@ -102,4 +94,3 @@ resource "aws_route" "public_internet_gateway" {
     create = "5m"
   }
 }
-
