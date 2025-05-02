02fd6b101cb0e10e9dfaa65352d7dc8894767ff9
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-02 15:54:39 +0530

    egr

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -119,14 +119,3 @@ resource "aws_internet_gateway" "this" {
   )
 }
 
-resource "aws_egress_only_internet_gateway" "this" {
-  count = local.create_vpc && var.create_egress_only_igw && local.max_subnet_length > 0 ? 1 : 0
-
-  vpc_id = local.vpc_id
-
-  tags = merge(
-    { "Name" = var.name },
-    var.tags,
-    var.igw_tags,
-  )
-}
