da929fe6c427fb959143336d7dc9dfe80585e70c
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-02 15:05:37 +0530

    check

--- main.tf
+++ main.tf
@@ -3,22 +3,11 @@ module "vpc" {
 
   name = "${var.environment}-${var.project_name}-vpc"
   cidr = var.vpc_cidr
-
   enable_dns_hostnames       = true
+  
+  public_subnets  = var.public_subnets
+  azs             = var.subnet_azs
 
-  tags = {
-    Terraform = "true"
-    Environment = "${var.environment}-${var.project_name}"
-  }
-}
-
-module "vpc2" {
-  source = "./modules/vpc"
-
-  name = "${var.environment}-${var.project_name}-vpc2"
-  cidr = "10.2.2.0/23"
-
-  enable_dns_hostnames       = true
 
   tags = {
     Terraform = "true"
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -25,3 +25,73 @@ resource "aws_vpc" "this" {
     var.vpc_tags,
   )
 }
+
+################################################################################
+# Publiс Subnets
+################################################################################
+
+locals {
+  create_public_subnets = local.create_vpc && local.len_public_subnets > 0
+}
+
+resource "aws_subnet" "public" {
+  count = local.create_public_subnets
+
+  availability_zone                              = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id                           = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
+  cidr_block                                     = element(concat(var.public_subnets, [""]), count.index)
+  vpc_id                                         = local.vpc_id
+
+  tags = merge(
+    {
+      Name = try(
+        var.public_subnet_names[count.index],
+        format("${var.name}-${var.public_subnet_suffix}-%s", element(var.azs, count.index))
+      )
+    },
+    var.tags,
+    var.public_subnet_tags,
+    lookup(var.public_subnet_tags_per_az, element(var.azs, count.index), {})
+  )
+}
+
+locals {
+  num_public_route_tables = var.create_multiple_public_route_tables ? local.len_public_subnets : 1
+}
+
+resource "aws_route_table" "public" {
+  count = local.create_public_subnets ? local.num_public_route_tables : 0
+
+  vpc_id = local.vpc_id
+
+  tags = merge(
+    {
+      "Name" = var.create_multiple_public_route_tables ? format(
+        "${var.name}-${var.public_subnet_suffix}-%s",
+        element(var.azs, count.index),
+      ) : "${var.name}-${var.public_subnet_suffix}"
+    },
+    var.tags,
+    var.public_route_table_tags,
+  )
+}
+
+resource "aws_route_table_association" "public" {
+  count = local.create_public_subnets ? local.len_public_subnets : 0
+
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
+  route_table_id = element(aws_route_table.public[*].id, var.create_multiple_public_route_tables ? count.index : 0)
+}
+
+resource "aws_route" "public_internet_gateway" {
+  count = local.create_public_subnets && var.create_igw ? local.num_public_route_tables : 0
+
+  route_table_id         = aws_route_table.public[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[0].id
+
+  timeouts {
+    create = "5m"
+  }
+}
+
