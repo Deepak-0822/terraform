3ac79c469a2825ce57804d587ecadd915b87bbce
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-02 14:20:19 +0530

    vpc2

--- main.tf
+++ main.tf
@@ -11,3 +11,17 @@ module "vpc" {
     Environment = "${var.environment}-${var.project_name}"
   }
 }
+
+module "vpc2" {
+  source = "./modules/vpc"
+
+  name = "${var.environment}-${var.project_name}-vpc2"
+  cidr = 10.2.2.2/23
+
+  enable_dns_hostnames       = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "${var.environment}-${var.project_name}"
+  }
+}
