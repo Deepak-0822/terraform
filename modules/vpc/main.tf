2e0f969fe88a65836a69c5a309afd50312f6d4a7
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-02 15:12:02 +0530

    ippo

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,7 +6,7 @@ locals {
     local.len_public_subnets
   )
     # Use `local.vpc_id` to give a hint to Terraform that subnets should be deleted before secondary CIDR blocks can be free!
-  vpc_id = try(aws_vpc_ipv4_cidr_block_association.this[0].vpc_id, aws_vpc.this[0].id, "")
+  vpc_id = try( aws_vpc.this[0].id, "")
 
   create_vpc = var.create_vpc
 }
