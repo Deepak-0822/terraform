7a4facaa903f6698658aa82ebd797f1d7389ff20
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-02 16:19:14 +0530

    ec2 sub

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ module "ec2_instance_image" {
   key_name               = "ec2-pem-key-mum"
   user_data = file("${path.module}/user_data.sh")
   #vpc_security_group_ids = [module.security_group.security_group_id] # Use the output name
-  subnet_id              = module.vpc.public_subnets[0]  # Access the first (or desired) public subnet ID from the list
+  subnet_id              = module.vpc.public_subnets[1]  # Access the first (or desired) public subnet ID from the list
 
   tags = {
     Terraform   = "true"
