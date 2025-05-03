c665b9bfa45f7d0ba09b253a88815deba2cadc5f
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-03 14:29:22 -0700

    ou

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,9 +1,4 @@
 output "id" {
   description = "The ID of the instance"
-  value = try(
-    aws_instance.this[0].id,
-    aws_instance.ignore_ami[0].id,
-    aws_spot_instance_request.this[0].id,
-    null,
-  )
+  value = try(aws_instance.this[0].id,  null)
 }
