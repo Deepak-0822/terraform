97b1e05f6e09a0b8bab12906a3461f29ab1362be
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-03 14:23:54 -0700

    alb pl

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
- backend "s3" {
-   bucket       = "demo-tfstate-testing"
-   key          = "path/to/state"
-   region       = "ap-south-1"
-   use_lockfile = true
-   encrypt = true 
- }
-}
