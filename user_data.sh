d10bdc3e2b9b03f4569308a4bc56506857aff485
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-06 06:28:41 -0700

    comi

--- user_data.sh
+++ user_data.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
-# Update and install Docker
+
 apt-get update -y
 apt-get install -y docker.io
- 
-# Enable and start Docker service
-systemctl enable docker
 systemctl start docker
-
-
-docker run -it -p 8080:80 \
+systemctl enable docker
+docker run -dit -p 80:80 \
   -e OPENPROJECT_SECRET_KEY_BASE=secret \
-  -e OPENPROJECT_HOST__NAME=0.0.0.0:8080 \
+  -e OPENPROJECT_HOST__NAME=0.0.0.0 \
+  -e OPENPROJECT_PORT=80 \
   -e OPENPROJECT_HTTPS=false \
-  -e OPENPROJECT_DEFAULT__LANGUAGE=en \
-  openproject/openproject:15
+  openproject/community:12
