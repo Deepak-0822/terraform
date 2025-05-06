b8e625601599b8c0b32ca95cdd3eda984c8c03ba
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-06 18:06:34 +0530

    Update main.tf

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,35 +1,35 @@
 resource "aws_lb" "this" {
   name               = var.name
-  internal           = var.internal
+  internal           = false
   load_balancer_type = "application"
   security_groups    = var.security_groups
   subnets            = var.subnets
 
-  enable_deletion_protection = var.enable_deletion_protection
+  enable_deletion_protection = false
 
   tags = var.tags
 }
 
 resource "aws_lb_target_group" "this" {
-  name     = var.target_group_name
-  port     = var.target_group_port
+  name     = "${var.name}-tg"
+  port     = var.target_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
 
   health_check {
     path                = "/"
     protocol            = "HTTP"
-    matcher             = "200-399"
+    matcher             = "200"
     interval            = 30
     timeout             = 5
-    healthy_threshold   = 2
+    healthy_threshold   = 5
     unhealthy_threshold = 2
   }
 
   tags = var.tags
 }
 
-resource "aws_lb_listener" "this" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
@@ -38,4 +38,11 @@ resource "aws_lb_listener" "this" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.this.arn
   }
+}
+
+resource "aws_lb_target_group_attachment" "ec2" {
+  count            = length(var.instance_ids)
+  target_group_arn = aws_lb_target_group.this.arn
+  target_id        = var.instance_ids[count.index]
+  port             = var.target_port
 }
