0e219c7ff58e738fae5b4cd701d44f50b350b818
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-07 17:42:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,38 @@
-module "s3" {
-  source         = "./modules/s3"
-  bucket_name    = var.bucket_name
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "iam" {
+  source = "./modules/iam"
+  name   = "image-processor-role"
 }
 
 module "sns" {
-  source         = "./modules/sns"
-  topic_name     = var.topic_name
+  source        = "./modules/sns"
+  topic_name    = "image-process-topic"
+  email_address = "notify@example.com"
 }
 
 module "lambda" {
-  source            = "./modules/lambda"
-  lambda_name       = var.lambda_name
-  bucket_name       = module.s3.bucket_name
-  sns_topic_arn     = module.sns.topic_arn
-  s3_notification_id = "image-upload"
+  source             = "./modules/lambda"
+  function_name      = "image-resizer"
+  iam_role_arn       = module.iam.lambda_role_arn
+  dest_bucket        = module.dest_bucket.bucket_name
+  sns_topic_arn      = module.sns.topic_arn
+  source_bucket_arn  = module.source_bucket.bucket_arn
+}
+
+module "source_bucket" {
+  source           = "./modules/s3_bucket"
+  name             = "image-source-bucket-123"
+  lambda_arn       = module.lambda.lambda_arn
+  lambda_permission = module.lambda.lambda_permission
+  prefix           = "uploads/"
+}
+
+module "dest_bucket" {
+  source = "./modules/s3_bucket"
+  name   = "image-destination-bucket-123"
+  lambda_arn = ""
+  lambda_permission = ""
 }
