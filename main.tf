1f5c7615430277e5c4a8750e4e4e210e7b720fe7
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-07 17:09:59 +0530

    Update main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "s3" {
+  source         = "./modules/s3"
+  bucket_name    = var.bucket_name
+}
+
+module "sns" {
+  source         = "./modules/sns"
+  topic_name     = var.topic_name
+}
+
+module "lambda" {
+  source            = "./modules/lambda"
+  lambda_name       = var.lambda_name
+  bucket_name       = module.s3.bucket_name
+  sns_topic_arn     = module.sns.topic_arn
+  s3_notification_id = "image-upload"
+}
