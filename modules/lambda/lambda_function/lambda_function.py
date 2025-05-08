0857e1a2b70660074f2a9464078f834b869b7ec8
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-08 12:33:10 +0530

    Update lambda_function.py

--- modules/lambda/lambda_function/lambda_function.py
+++ modules/lambda/lambda_function/lambda_function.py
@@ -4,6 +4,7 @@ import os
 from PIL import Image
 
 s3_client = boto3.client('s3')
+DEST_BUCKET = os.environ['DEST_BUCKET']  # New line
 
 def resize_image(image_path, resized_path):
     with Image.open(image_path) as image:
@@ -13,19 +14,18 @@ def resize_image(image_path, resized_path):
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
     key    = event['Records'][0]['s3']['object']['key']
-    
-    # Get only the file name
+
     file_name = os.path.basename(key)
-    
-    # Create temp paths
     download_path = f'/tmp/{uuid.uuid4()}_{file_name}'
     upload_path = f'/tmp/resized-{file_name}'
-    
-    # Download, resize, upload
+
     s3_client.download_file(bucket, key, download_path)
     resize_image(download_path, upload_path)
-    s3_client.upload_file(upload_path, bucket, f'resized/{file_name}')
+    
+    # Upload to destination bucket
+    s3_client.upload_file(upload_path, DEST_BUCKET, f'resized/{file_name}')
+    
     return {
         'statusCode': 200,
-        'body': 'Image resized successfully!'
+        'body': 'Image resized and uploaded successfully!'
     }
