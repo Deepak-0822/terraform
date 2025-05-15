50ad00ed3844ce786eb6938e315b7fa23cea8bb9
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-15 13:33:51 +0530

    p

--- modules/apigateway/main.tf
+++ modules/apigateway/main.tf
@@ -6,8 +6,9 @@ resource "aws_apigatewayv2_api" "api" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = aws_apigatewayv2_api.api.id
   integration_type = "AWS_PROXY"
+  integration_uri  = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:${var.lambda_function_name}/invocations"
+  payload_format_version = "2.0"
   integration_method = "POST"
-  integration_uri  = "arn:aws:lambda:ap-south-1:971422676158:function:my-container-lambda"
 }
 
 
