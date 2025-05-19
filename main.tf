983c8b24df28c4f7e8d02fdcaf3c7a363cc10aaa
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-19 16:18:32 +0530

    sg

--- main.tf
+++ main.tf
@@ -25,11 +25,16 @@ module "ec2_instance_ngnix" {
   instance_type          = var.instance_type
   key_name               = "Test"
   user_data = file("${path.module}/ngnix_data.sh")
-  #vpc_security_group_ids = [module.security_group.security_group_id] # Use the output name
+  vpc_security_group_ids = [module.sg.sg_id] # Use the output name
   subnet_id              = module.vpc_ngnix.public_subnets[1]  # Access the first (or desired) public subnet ID from the list
 
   tags = {
     Terraform   = "true"
     Environment = "${var.environment}-${var.project_name}"
   }
+}
+
+module "sg" {
+  source  = "./modules/security_group"
+  vpc_id  = module.vpc_ngnix.vpc_id
 }
