ea3f9e0a561a444744e474f896b568e4d1d93c2b
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-20 13:35:20 +0530

    sa

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -23,23 +23,6 @@ variable "secret_values" {
   default   = {}
 }
 
-variable "sg_name" {}
-
-variable "sg_description" {}
-
-variable "sg_vpc_id" {}
-
-variable "sg_ingress_rules" {
-  type        = map
-}
-
-variable "sg_tags" {
-  type = map(string)
-  default = {
-    "terraform" = "True"
-  }
-}
-
 variable "rds_instance_identifier" {
   type = map(string)
 }
@@ -71,6 +54,7 @@ variable "rds_instance_storage_encrypted" {
 
 variable "rds_instance_kms_key_id" {
   type = string
+  default = null
 }
 
 variable "rds_instance_db_name" {
