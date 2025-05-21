4c92766ad9fc9f7398bce836d2b6423a143d9d65
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-21 17:00:46 +0530

    yhf

--- main.tf
+++ main.tf
@@ -1,6 +1,12 @@
+module "iam_lambda" {
+  source         = "./modules/iam"
+  lambda_role_name = var.lambda_role_name
+}
+
 module "lambda_hello" {
   source               = "./modules/lambda"
   function_name        = "lambda_hello"
   lambda_zip_path      = "./lambda_function.zip"
+  role_arn             = module.iam_lambda.role_arn
 }
 
--- modules/lambda/ouputs.tf
+++ modules/lambda/ouputs.tf
@@ -1,3 +1,3 @@
 output "lambda_name" {
-  value = aws_lambda_function.container_lambda.function_name
+  value = aws_lambda_function.this.function_name
 }
