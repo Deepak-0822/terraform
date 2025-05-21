df8e533caf33bef7a45bc4736a92f2dcd90313e2
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-05-21 17:03:21 +0530

    vjv

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "this" {
   filename         = var.lambda_zip_path
   function_name    = var.function_name
-  role             = aws_iam_role.lambda_role.arn
+  role          = var.role_arn
   handler          = var.handler
   runtime          = var.runtime
   source_code_hash = filebase64sha256(var.lambda_zip_path)
