c8862585fbf003991629aa8d4110987cff14865c
Deepak-0822 <140145819+Deepak-0822@users.noreply.github.com>
2025-06-21 05:33:00 +0000

    remove

--- data-stack/modules/rds/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "name" {}
-variable "subnet_ids" {
-  type = list(string)
-}
-variable "instance_type" {}
-variable "db_user" {}
-variable "db_password" {}
-variable "db_name" {}
-variable "sg_id" {}
